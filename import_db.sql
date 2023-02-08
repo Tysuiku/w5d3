7784c5307d57feac15f1f7f5e871ea1ab91ecd05
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-02-08 12:13:40 -0500

    seeding done

--- import_db.sql
+++ import_db.sql
@@ -35,24 +35,12 @@ CREATE TABLE question_likes (
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(question_id) REFERENCES questions(id)
 );
+
+
 INSERT INTO users(fname, lname)
 VALUES ('TIM', 'DONG'),
-    ('ZAHIN', 'BH');
+       ('ZAHIN', 'BH');
+
 INSERT INTO questions(title, body, author_id)
-VALUES (
-        'coding',
-        'we are having fun',
-(
-            SELECT id
-            FROM users
-            WHERE fname = 'TIM'
-        )
-    ) (
-        'computers',
-        'need help with troubleshoot',
-(
-            SELECT id
-            FROM users
-            WHERE lname = 'BH'
-        )
-    );
+VALUES ('coding','we are having fun',(SELECT id FROM users WHERE fname = 'TIM')), 
+('computers','need help with troubleshoot',(SELECT id FROM users WHERE lname = 'BH'));
